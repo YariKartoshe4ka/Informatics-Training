0ea424340892619ddc5cb6de5e32f0b16546c80d
YariKartoshe4ka <yaroslav.kikel.06@inbox.ru>
2021-03-28 17:49:19 +0300

    Добавление приветственного экрана

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, request, render_template
+
+
+app = Flask(__name__, static_folder='static')
+
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index_page():
+    return render_template('index.html')

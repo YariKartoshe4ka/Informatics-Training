564fcfb22e0d667f045c5349a9bc3421a549667f
YariKartoshe4ka <yaroslav.kikel.06@inbox.ru>
2021-03-29 08:34:39 +0300

    Добавление выбора типа тренировки (только для степеней 2)

--- main.py
+++ main.py
@@ -7,3 +7,14 @@ app = Flask(__name__, static_folder='static')
 @app.route('/')
 def index_page():
     return render_template('index.html')
+
+
+@app.route('/choose', methods=['GET', 'POST'])
+def choose_page():
+    if request.method == 'GET':
+        return render_template('choose.html')
+
+    if request.form.get('type') == 'powers':
+        return render_template('powers.html', max_power=request.form.get('range'))
+
+    return render_template('choose.html', error='Проверьте правильность заполнения полей!') 
---
+++ static/js/choose.js
@@ -0,0 +1,14 @@
+$('#select').on('change', function() {
+    if ($('#select').val() == 'powers') {
+        $('#powers').css({'display': 'block'});
+        $('#systems').css({'display': 'none'});
+    } else {
+        $('#powers').css({'display': 'none'});
+        $('#systems').css({'display': 'block'});
+    }
+});
+
+
+$('#range').on('change', function() {
+    $('#label-range').text('Максимальная степень: ' + $('#range').val());
+});

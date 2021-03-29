3dec3078ebaeb4317cc714397a3b6bf081b86d59
YariKartoshe4ka <yaroslav.kikel.06@inbox.ru>
2021-03-29 19:33:11 +0300

    Завершение написания тренировки по степеням и добавление конфига p4a

--- static/js/powers.js
+++ static/js/powers.js
@@ -1,23 +1,38 @@
 function generate_task(max_power) {
     var power = 1 + Math.floor(Math.random() * max_power);
     
-    $('#task').html(`2<sup>${power}</sup>`);
-    $('#answer').val(Math.pow(2, power));
+    $('#task').text(power);
+
+    sessionStorage.setItem('answer', Math.pow(2, power));
+
+    if (!sessionStorage.getItem('all_attemps')) {
+        sessionStorage.setItem('all_attemps', 0);
+        sessionStorage.setItem('correct_attemps', 0);
+    }
 };
 
 function check_answer(answer) {
-    var true_answer = $('#answer').val();
+    var correct_answer = sessionStorage.getItem('answer');
+    var all_attemps = parseInt(sessionStorage.getItem('all_attemps'));
+    var correct_attemps = parseInt(sessionStorage.getItem('correct_attemps'));
 
-    if (answer == true_answer) {
+    sessionStorage.setItem('all_attemps', ++all_attemps);
+
+    if (answer == correct_answer) {
         $('#result').text('Bерно!');
-        $('#result').css({'display': 'block'});
+        sessionStorage.setItem('correct_attemps', ++correct_attemps);
     }
-    else {
-        $('#result').html(`Неверно! 2<sup>${Math.log2(true_answer)}</sup>=${true_answer}`);
-        $('#result').css({'display': 'block'});
-    }
+
+    else
+        $('#result').html(`Неверно! 2<sup>${Math.log2(correct_answer)}</sup>=${correct_answer}`);
+        
+    $('#result').css({'display': 'block'});
+
+    
+
+    $('#statistics').text(`Bерные попытки: ${correct_attemps} из ${all_attemps} (${parseInt(correct_attemps / all_attemps * 100)}%)`);
 
     generate_task(window.max_power);
-}
+};
 
 generate_task(max_power);

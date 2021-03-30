76cb9057290d7dc782883b670e5a6fd0a3bebee5
YariKartoshe4ka <yaroslav.kikel.06@inbox.ru>
2021-03-30 17:11:09 +0300

    Добавление верстки систем счисления

--- main.py
+++ main.py
@@ -15,9 +15,20 @@ def choose_page():
         return render_template('choose.html')
 
     if request.form.get('type') == 'powers':
-        return render_template('powers.html', max_power=int(request.form.get('range')))
+        range = int(request.form.get('range', 0))
 
-    return render_template('choose.html', error='Проверьте правильность заполнения полей!')
+        if 10 <= range <= 20:
+            return render_template('powers.html', max_power=range)
+
+        return render_template('choose.html', error='Проверьте правильность заполнения полей!')
+
+    system_from = int(request.form.get('system-from', 0))
+    system_to = int(request.form.get('system-to', 0))
+
+    if 2 <= system_from <= 36 and 2 <= system_to <= 36:
+        return render_template('systems.html', system_from=system_from, system_to=system_to)
+
+    return render_template('choose.html', error='Проверьте правильность заполнения полей!') 
 
 
 if __name__ == '__main__':

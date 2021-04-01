2f80ee441d8ebbdcb44261f43a83589aa14afb12
YariKartoshe4ka <yaroslav.kikel.06@inbox.ru>
2021-04-01 08:29:13 +0300

    Исправление бага с 500ой шибкой при пустой форме систем счисления, добавление кнопки назад

--- main.py
+++ main.py
@@ -22,8 +22,8 @@ def choose_page():
 
         return render_template('choose.html', error='Проверьте правильность заполнения полей!')
 
-    system_from = int(request.form.get('system-from', 0))
-    system_to = int(request.form.get('system-to', 0))
+    system_from = int(request.form.get('system-from') or 0)
+    system_to = int(request.form.get('system-to') or 0)
 
     if 2 <= system_from <= 36 and 2 <= system_to <= 36 and system_from != system_to:
         return render_template('systems.html', system_from=system_from, system_to=system_to)
